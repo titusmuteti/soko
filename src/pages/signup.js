99b2679e279f1b0324f78adb30088d32e2494585
Titus Mumo <titusmumo@gmail.com>
2023-11-09 21:33:39 +0300

    adding signup button functionality

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,35 +1,112 @@
-import React, { useState } from 'react';
-import { toast } from 'react-toastify';
+import React, { useState, useEffect } from 'react';
 import soko from '../assests/images/soko.png';
-import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 import CitySelector from '../components/CitySelector';
-
 
 function Signup() {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [region, setRegion] = useState('');
-  const [city, setCity] = useState([]);
+  const [city, setCity] = useState('');
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  function handleLogin(event) {
-    event.preventDefault();
-    
-  }
+  const regionOptions = [
+    "Bomet", "Eldoret", "Homabay", "Kajiado", "Kakamega", "Kiambu", "Kilifi", "Kisumu",
+    "Kitui", "Liaikipia", "Machakos", "Makueni", "Mombasa", "Muranga", "Nairobi", "Nakuru",
+    "Narok", "Nyeri", "TransNzoia", "Vihiga"
+  ];
 
-  const regionOptions = ["Bomet", "Eldoret", "Homabay", "Kajiado", "Kakamega", "Kiambu", "Kilifi", "Kisumu", 
-  "Kitui", "Liaikipia", "Machakos", "Makueni", "Mombasa","Muranga", "Nairobi", "Nakuru", "Narok", "Nyeri", "TransNzoia", "Vihiga"];
+  const handleSignup = () => {
+    const validationErrors = [];
 
-  const handleLocationChange = (event) => {
-    event.preventDefault();
-    const selectedValue = event.target.value;
+    if (!email) {
+      validationErrors.push('Email is required.');
+    }
+
+    if (!password) {
+      validationErrors.push('Password is required.');
+    } else if (password.length < 6) {
+      validationErrors.push('Password should be at least 6 characters long.');
+    }
+
+    if (!firstName) {
+      validationErrors.push('First name is required.');
+    }
+
+    if (!lastName) {
+      validationErrors.push('Last name is required.');
+    }
+
+    if (!phoneNumber) {
+      validationErrors.push('Phone number is required.');
+    }
+
+    if (!region) {
+      validationErrors.push('Region is required.');
+    }
+
+    if (!city) {
+      validationErrors.push('City is required.');
+    }
+
+    setErrors(validationErrors);
+
+    if (validationErrors.length === 0) {
+      setIsLoading(true);
+    }
   };
+
+  useEffect(() => {
+    if (isLoading) {
+      const userData = {
+        user: {
+          first_name: firstName,
+          last_name: lastName,
+          phone_number: phoneNumber,
+          email,
+          password,
+          addresses_attributes: [
+            { city: city, region: region },
+          ],
+        },
+      };
+      
+      
+
+      fetch('https://sokoapi.onrender.com/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(userData),
+      })
+        .then(response => {
+          setIsLoading(false);
+
+          if (response.ok) {
+            window.location.href = '/login'; 
+          } else {
+            response.json().then(data => {
+              if (data.errors) {
+                setErrors(data.errors);
+              } else {
+                setErrors(['An unexpected error occurred.']);
+              }
+            });
+          }
+        })
+        .catch(error => {
+          setIsLoading(false);
+          setErrors(['Network error.']);
+        });
+    }
+  }, [isLoading]);
 
   const handleRegionChange = (event) => {
     const selectedRegion = event.target.value;
@@ -114,7 +191,7 @@ function Signup() {
               </div>
             </Form.Group>
 
-            <Button variant="primary" className="mt-3 w-100 mt-5" onClick={handleLogin} disabled={isLoading}>
+            <Button variant="primary" className="mt-3 w-100 mt-5" onClick={handleSignup} disabled={isLoading}>
               {isLoading ? "Loading..." : "SIGNUP"}
             </Button>
           </Form>
